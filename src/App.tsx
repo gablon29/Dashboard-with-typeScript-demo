8a5f186b6eb9cdd4f38aa895ebf76f7fac110056
gablon29 <gabriellondero40@gmail.com>
2023-09-01 17:28:42 -0300

    creando componentes navBar/footer/menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,52 @@ import {
   RouterProvider,
   Route,
   Link,
+  Outlet,
 } from "react-router-dom";
 import Users from "./pages/users/Users";
 import { Products } from "./pages/products/Products";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "users",
-    element: <Users />,
-  },
-  {
-    path: "products",
-    element: <Products />,
-  },
-]);
+import { NavBar } from "./components/navBar/NavBar";
+import { Menu } from "./components/menu/Menu";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <NavBar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentConteiner">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 }
 
