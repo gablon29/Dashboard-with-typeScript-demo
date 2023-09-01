3b969b4a33560f2753ba6f01f2687f2cbf509d04
gablon29 <gabriellondero40@gmail.com>
2023-09-01 17:00:43 -0300

    creando el enrutado principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import Home from "./pages/home/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Users from "./pages/users/Users";
+import { Products } from "./pages/products/Products";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "users",
+    element: <Users />,
+  },
+  {
+    path: "products",
+    element: <Products />,
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
