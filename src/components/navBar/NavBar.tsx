0ff4745f31a6b0d887078b97f579c8ca4e9dc98a
gabri <gabriellondero40@gmail.com>
2023-09-02 08:53:02 -0300

    desarrollando el componente navbar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,5 +1,27 @@
 import "./navBar.scss";
 
 export const NavBar = () => {
-  return <div className="navBar">NavBar</div>;
+  return (
+    <div className="navBar">
+      <div className="logo">
+        <img src="logo.svg" alt="logos" />
+        <span>Dashboard</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="/noavatar.png" alt="avatar" />
+          <span>User Name</span>
+        </div>
+        <img src="/setting.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
+//noavatar.png
