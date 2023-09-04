39bdc471019d87a857cb8069a7b92cf6539939ae
gabri <gabriellondero40@gmail.com>
2023-09-04 07:11:23 -0300

    creando el footer y el navbar

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
 import "./menu.scss";
 
 export const Menu = () => {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      <div className="item">
+        <span className="title">Main</span>
+        <Link to="/" className="listItem">
+          <img src="/home.svg" alt="home" />
+          <span className="listItemTitle">Home</span>
+        </Link>
+        <Link to="/" className="listItem">
+          <img src="/profile.svg" alt="profile" />
+          <span className="listItemTitle">Profile</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
