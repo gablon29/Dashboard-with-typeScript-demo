ab6994dd4c2b3cd5d7a45937faa4fbbb9c63f240
gabri <gabriellondero40@gmail.com>
2023-09-06 18:22:36 -0300

    creacion del componente de pieChartBox

---
+++ src/components/pieChartBox/PieChartBox.tsx
@@ -0,0 +1,11 @@
+import "./pieChartBox.scss";
+function PieChartBox() {
+  return (
+    <div className="pieChartBox">
+      <h1>Titulo</h1>
+      <div className="chart"></div>
+    </div>
+  );
+}
+
+export default PieChartBox;
