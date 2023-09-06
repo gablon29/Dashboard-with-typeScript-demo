8c364d0f85fdb287b1c0650769889c21140e73ed
gabri <gabriellondero40@gmail.com>
2023-09-06 18:14:24 -0300

    componente barChartBox terminado

--- src/components/barchartBox/BarChartBox.tsx
+++ src/components/barchartBox/BarChartBox.tsx
@@ -10,14 +10,15 @@ type Props = {
 
 const BarChartBox = (props: Props) => {
   return (
-    <div className="barCharBox">
+    <div className="barChartBox">
       <h1>{props.title}</h1>
       <div className="chart">
-        <ResponsiveContainer width="100%" height="100%">
+        <ResponsiveContainer width="100%" height={150}>
           <BarChart width={150} height={40} data={props.chartData}>
             <Tooltip
               contentStyle={{ background: "#2a3447", borderRadius: "5px" }}
               labelStyle={{ display: "none" }}
+              cursor={{ fill: "none" }}
             />
             <Bar dataKey={props.dataKey} fill={props.color} />
           </BarChart>
