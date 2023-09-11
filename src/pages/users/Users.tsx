90ea2ae720f6607909d52ee476231e80bd5f640a
gabri <gabriellondero40@gmail.com>
2023-09-11 07:55:13 -0300

    terminando el componente de dataTable para listar a los usuarios

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,66 @@
 import DataTable from "../../components/dataTable/DataTable";
+import { userRows } from "../../data";
 import "./users.scss";
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "actions",
+    headerName: "Actions",
+    width: 100,
+    renderCell: (params) => {
+      return (
+        <div className="action">
+          <div className="view">View</div>
+          <div className="delete">Delete</div>
+        </div>
+      );
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 100,
+    type: "boolean",
+  },
+];
 function Users() {
   return (
     <div className="users">
@@ -7,7 +68,7 @@ function Users() {
         <h1>Users</h1>
         <button>Add New User</button>
       </div>
-      <DataTable />
+      <DataTable slug="users" columns={columns} rows={userRows} />
     </div>
   );
 }
