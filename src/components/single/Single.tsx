1c5daecbad8405b95545341a932868d078a43013
gabri <gabriellondero40@gmail.com>
2023-09-19 06:45:32 -0300

    creando el renderizado del componente single

--- src/components/single/Single.tsx
+++ src/components/single/Single.tsx
@@ -1,7 +1,15 @@
 import "./single.scss";
 
 const Single = () => {
-  return <div className="single"></div>;
+  return (
+    <div className="single">
+      <div className="view">
+        <div className="info"></div>
+        <div className="chart"></div>
+      </div>
+      <div className="activities"></div>
+    </div>
+  );
 };
 
 export default Single;
