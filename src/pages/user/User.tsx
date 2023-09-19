6d6d914bd231f047dfd2956d01bd6e85120dfa32
gabri <gabriellondero40@gmail.com>
2023-09-19 07:37:01 -0300

    estructurando el componente de simpleChart dentro del componente single

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,7 +1,12 @@
+import Single from "../../components/single/Single";
 import "./user.scss";
 
 const User = () => {
-  return <div className="user"></div>;
+  return (
+    <div className="user">
+      <Single />
+    </div>
+  );
 };
 
 export default User;
