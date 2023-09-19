32a153834f8e0a7d62060b109d4d41fa89cfa1e1
gabri <gabriellondero40@gmail.com>
2023-09-19 06:53:11 -0300

    estruturando la logica del Componente Single

--- src/components/single/Single.tsx
+++ src/components/single/Single.tsx
@@ -4,7 +4,19 @@ const Single = () => {
   return (
     <div className="single">
       <div className="view">
-        <div className="info"></div>
+        <div className="info">
+          <div className="topInfo">
+            <img src="" alt="" />
+            <h1>Jonh Cuanso</h1>
+            <button>Update</button>
+          </div>
+          <div className="detail">
+            <div className="item">
+              <span className="itemTitle">Username:</span>
+              <span className="itemValue">Jonh Cuanzo</span>
+            </div>
+          </div>
+        </div>
         <div className="chart"></div>
       </div>
       <div className="activities"></div>
